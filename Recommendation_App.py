5c0fa9db2ce407b7404ee8e3139b090483f0de93
RAGHAVENDRA KN <135142090+Raghavendra0827@users.noreply.github.com>
2024-01-25 12:03:45 +0530

    Update Recommendation_App.py

--- Recommendation_App.py
+++ Recommendation_App.py
@@ -10,8 +10,9 @@ from book_recommendation_final import Recommend
 def main():
     st.title("Book Recommendation System")
 
-    # Get user input for book genre
-    user_id = st.number_input("User ID")
+    # Get user input for user id
+    user_id = st.number_input("User ID", value=27672)
+    
     Recommend(user_id)
 
 if __name__ == "__main__":
